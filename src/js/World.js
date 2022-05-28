6fd6cc9a382a889dbb8522868719cc82a71542fd
jeromehaas <hello@jeromehaas.ch>
2022-05-28 20:16:57 +0200

    Setup: frontend styles and animations

--- src/js/World.js
+++ src/js/World.js
@@ -1,135 +1,75 @@
-import * as THREE from 'three';
-import { Mesh } from 'three';
-import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
-import { gsap } from 'gsap';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
-import * as dat from 'dat.gui';
+import { gsap } from "gsap";
 
-class World {
+class Background {
 
 	constructor() {
-		this.canvas = document.querySelector('canvas.background');
-		this.scene = new THREE.Scene();	
-		this.clock = new THREE.Clock();
-		this.camera = null;
-		this.raycaster = null;
-		this.controls = null;
-		this.debugger = null;
-		this.gui = null;
-		this.light = null;
-		this.loaders = {
-			draco: null,
-			gltf: null,
-			mixer: null
+		this.name = 'background';
+		this.preview = {
+			small: document.querySelectorAll('.preview__item--big .content__item'),
+			big: document.querySelectorAll('.preview__item--small .content__item'),
+		},
+		this.images = document.querySelectorAll('.item__image');
+		this.background = {
+			body: document.querySelector('body'),
+			triangle: document.querySelectorAll('.triangle__item'),
+			squares: document.querySelectorAll('.background__square')
 		}
-		this.meshes = {
-			cube: {
-				material: null,
-				geomerty: null,
-				mesh: null
-			}
-		};
-		this.configs = {
-			width: this.canvas.clientWidth,
-			height: this.canvas.clientWidth
-		};
 	}
 
 	init = () => {
-		this.initCamera();
-		this.initLoaders();
-		this.initControls();
-		// this.createCube();
-		this.initGUI();
-		this.resizer();
-		this.initLight();
-		this.initModel();
-		this.render();
-		this.animate();
-	};
+		if (!document.querySelector(`.js-${this.name}`)) return false;
+		this.setupSquareAnimation()
+		this.setupPreviewAnimation();
+		this.setupColorSwitcher();
 
-	initGUI = () => {
-		this.gui = new dat.GUI();
-		const camera = this.gui.addFolder('Camera');
-		const position = camera.addFolder('Position');
-		const rotation = camera.addFolder('Rotation');
-		position.add(this.camera.position, 'x').min(-100).max(100);
-		position.add(this.camera.position, 'y').min(-100).max(100);
-		position.add(this.camera.position, 'z').min(-100).max(100);
-		rotation.add(this.camera.rotation, 'x').min(-10).max(10).step(0.1);
-		rotation.add(this.camera.rotation, 'y').min(-100).max(100);
-		rotation.add(this.camera.rotation, 'z').min(-100).max(100);
-		console.log(this.camera)
 	}
 
-	initLight = () => {
-		this.light = new THREE.AmbientLight(0xffffff, 1);
-		this.light.position.x = 5;
-		this.light.position.y = 5;
-		this.light.position.z = 15;
-		this.scene.add(this.light);
-		const helper = new THREE.PointLightHelper(this.light)
-		this.scene.add(helper);
-	};
-
-	initLoaders = () => {
-		this.loaders.draco = new DRACOLoader();
-		this.loaders.draco.setDecoderPath('three/draco/');
-		this.loaders.gltf = new GLTFLoader();
-		this.loaders.gltf.setDRACOLoader(this.loaders.draco);
-	};
-
-	initModel = () => {
-		this.loaders.gltf.load('three/models/world/world.glb', 
-			(gltf) => {
-				console.log(gltf);
-				this.scene.add(gltf.scene)
-			}
-		)
-	};
-
-	initControls = () => {
-		this.controls = new TrackballControls(this.camera, this.canvas);
-		this.controls.enableDamping = true;
-	}
-
-
-	resizer = () => {
-		window.addEventListener('resize', () => {
-			this.configs.widht = this.canvas.clientWidth,
-			this.configs.height = this.canvas.clientWidth,	
-			this.initCamera();
-			this.render();
-			this.initControls();
+	setupColorSwitcher = () => {
+		this.images.forEach((image) => {
+			image.addEventListener('click', (event) => this.switchBackgroundColor(event))
 		});
+		this.switchBackgroundColor('purple');
 	};
 
-	initCamera = () => {
-		this.camera = new THREE.PerspectiveCamera(75, this.configs.width / this.configs.height, 1, 1000);
-		this.camera.position.z = 15;
-		this.camera.rotateOnAxis.y  = 90 * Math.PI;
-	};
+	setupSquareAnimation = () => {
+		const timeline = gsap.timeline({ repeat: -1, yoyo: true, ease: 'linear' });
+		timeline.to(this.background.squares, { y: 30, duration: 6 });
+	}
 
-	initControls = () => {
-		this.controls = new TrackballControls(this.camera, this.canvas);
-		this.controls.enableDamping = true;
-	};
+	setupPreviewAnimation = () => {
+		console.log(this.preview.small);
+		const timeline = gsap.timeline();
+		timeline.to(this.preview.small, { y: -16, opacity: 1 , duration: 1, stagger: 0.1 });
+		timeline.to(this.preview.big, { y: -16, opacity: 1 , duration: 1, stagger: 0.1 });
+	}
 
-
-	render = () => {
-		this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
-		this.renderer.setClearColor( 0x000000, 0 );
-		this.renderer.setSize(this.configs.width, this.configs.height);
-		this.renderer.render(this.scene, this.camera);
-	};
-
-	animate = () => {
-		this.controls.update();
-		this.renderer.render(this.scene, this.camera);
-		window.requestAnimationFrame(this.animate);
-	};
+	switchBackgroundColor = (event) => {
+		const color = event.target ? event.target.dataset.color : 'purple';
+		switch (color) {
+			case 'yellow': {
+				gsap.to(this.background.body, { backgroundColor: '#FAD961', duration: 0.3 });
+				gsap.to(this.background.triangle, { backgroundImage: "linear-gradient(270deg, rgba(249, 82, 197, 0.75) 0%, rgba(153, 82, 255, 0.0) 80%)", duration: 0.3 })
+				gsap.to(this.background.squares, { backgroundImage: "linear-gradient(135deg, rgba(249, 82, 197, 0.75) 0%, rgba(153, 82, 255, 0.0) 80%)", duration: 0.3 })
+				break;
+			}
+			case 'cyan': {
+				gsap.to(this.background.body, { backgroundColor: '#007DFA', duration: 0.3 });
+				gsap.to(this.background.triangle, { backgroundImage: "linear-gradient(270deg, rgba(0, 199, 250, 0.75) 0%, rgba(0, 125, 250, 0.0) 80%)", duration: 1 })
+				gsap.to(this.background.squares, { backgroundImage: "linear-gradient(135deg, rgba(0, 199, 250, 0.75) 0%, rgba(0, 125, 250, 0.0) 80%)", duration: 1 })
+				break;
+			}
+			case 'purple': {
+				gsap.to(this.background.body, { backgroundColor: '#684BB1', duration: 0.3 });
+				gsap.to(this.background.triangle, { backgroundImage: "linear-gradient(270deg, rgba(249, 82, 197, 0.75) 0%, rgba(153, 82, 255, 0.0) 80%)", duration: 1 })
+				gsap.to(this.background.squares, { backgroundImage: "linear-gradient(135deg, rgba(249, 82, 197, 0.75) 0%, rgba(153, 82, 255, 0.0) 80%)", duration: 1 })
+				break;
+			}
+			default: {
+				console.log('its not purple');
+			}
+		}
+	}
 
 }
 
-export default World;
+export default Background;
